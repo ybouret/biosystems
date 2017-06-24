6fc05fe1adae8e3300b768b7e9e562a587b6d0e2
ybouret <yann.bouret@gmail.com>
2017-06-24 19:02:44 +0200

    upgrading code

--- src/fish/arduino/fish_command_v2/fish_command_v2.ino
+++ src/fish/arduino/fish_command_v2/fish_command_v2.ino
@@ -1,4 +1,6 @@
 #include <Servo.h>
+#include <math.h>
+
 //______________________________________________________________________________
 //
 //
@@ -6,6 +8,16 @@
 //
 //______________________________________________________________________________
 unsigned long     baudrate       = 115200;
+#define           pin_servo        9
+
+#if 0
+const int         pin_servo      = 9;
+int      pin_interFrame     = 12;
+int      pin_forceSensor    = 1;
+int      pin_tensionEntree  = 3;
+int      pin_tensionSortie  = 2;
+#endif
+
 
 
 //______________________________________________________________________________
@@ -14,13 +26,15 @@ unsigned long     baudrate       = 115200;
 // Simulation parameters, user defined
 //
 //______________________________________________________________________________
-
+Servo    myservo;
 float    frequency          = 1.0;     // servo frequency, in Hz
-float    amplitude          = 100.0;   // in degrees
+float    amplitude          = 45.0;   // in degrees
 unsigned num_periods        = 5;       // number of servo periods during motion
 unsigned points_per_period  = 20;      // number of saved points per cycle
 float    resting_time       = 0.5;     // resting initial time
 unsigned points_during_rest = 10;      // points to save during resting time
+float    angle_init         = 90.0f;
+float    angle_shift        =  0.0f;
 
 //______________________________________________________________________________
 //
@@ -34,10 +48,8 @@ unsigned iter         = 0;
 
 float period          = 1.0/frequency;
 float dt_swim         = period/points_per_period;
-const float       pi  = 3.1415926535;
-const float omega     = 2.0*pi*frequency;
-float simulation_time = resting_time + num_periods * period;
-
+const float omega     = 2.0*M_PI*frequency;
+const float start_angle = angle_init+angle_shift;
 
 
 
@@ -47,23 +59,48 @@ float             t              = 0;
 float             t_last         = 0;
 float             t_save         = 0.25;
 
+bool              resting     = true;
+float             t_swim      = 0;
+
+
 #define TSYS()       (micros())
 #define TSYS2TIME(T) (1e-6* ((float)(T)))
+
+
+// write resting position
+void myservo_rests()
+{
+    myservo.write(angle_init);
+}
+
+// write swimming position
+void myservo_swims()
+{
+    // ellapsed time since swimming order
+    const float t_run = t - t_swim;
+    const float angle = amplitude * sin( omega * t_run ) + start_angle;
+    myservo.write( (int) angle );
+}
 
 //______________________________________________________________________________
 //
 //
-// Initialisation
+// Initialisation: probes, servo and Python info
 //
 //______________________________________________________________________________
 void setup()
 {
+    // communication settings
     Serial.begin(baudrate);
+    
+    // initialize servo
+    myservo.attach(pin_servo,900,2100);
+    myservo_rests();
+    delay(500);
     
     // initialize
     tsys_start = TSYS();
     t_last     = TSYS2TIME(tsys_start);
-    //Serial.println(tsys_start);
     
     // sending python #total number of points
     Serial.println(num_iter);
@@ -117,8 +154,16 @@ void loop()
     }
     else
     {
+        
         if(iter<num_iter)
         {
+            
+            if(resting)
+            {
+                t_swim  = t;
+                resting = false;
+            }
+            
             //__________________________________________________________________
             //
             //
@@ -127,6 +172,8 @@ void loop()
             //__________________________________________________________________
             if(t-t_last>=dt_swim)
             {
+                // initialize t_swim for servo position
+               
                 // emit a swimming point
                 performIO("swimming");
                 

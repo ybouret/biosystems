bd781988a174431da93d7438c94d5828e069fcd9
ybouret <yann.bouret@gmail.com>
2017-10-13 10:04:58 +0200

    WIP, will check second order

--- src/lithium/sim8.cpp
+++ src/lithium/sim8.cpp
@@ -30,17 +30,17 @@ public:
     odeint(1e-7),
     lambda(NVAR),
     mu(NVAR,NVAR),
-    rho_h(0),
-    rho_7(0.8)
+    rho_h(2),
+    rho_7(0)
     {
         odeint.start(NVAR);
         lambda[EH]  = 1;
         lambda[Li6] = 1;
-        lambda[Li7] = 1;
+        lambda[Li7] = 3;
 
-        mu[1][1] = 1+rho_h;     mu[1][2] = 1.0-rho_7; mu[1][3] = rho_7;
+        mu[1][1] = 1+rho_h;     mu[1][2] = 1.0-rho_7;   mu[1][3] = rho_7;
         mu[2][1] = lambda[Li6]; mu[2][2] = lambda[Li6]; mu[2][3] = 0;
-        mu[3][1] = lambda[Li7]; mu[3][2] = 0; mu[3][2] = lambda[Li7];
+        mu[3][1] = lambda[Li7]; mu[3][2] = 0;           mu[3][3] = lambda[Li7];
 
     }
 
@@ -60,16 +60,18 @@ private:
 
 YOCTO_PROGRAM_START()
 {
-    LiSystem       LiSys;
+    LiSystem       LS;
     vector<double> Z(NVAR);
+    std::cerr << "lam=" << LS.lambda << std::endl;
+    std::cerr << "mu="  << LS.mu  << std::endl;
 
     ios::wcstream fp("output.dat");
     fp("%g %g %g %g\n", 0.0, 0.0, 0.0, 0.0);
     double dtau = 0.001;
-    for(double tau=0;tau<=10;tau+=0.001)
+    for(double tau=0;tau<=100;tau+=0.01)
     {
-        double ctrl = 0;
-        LiSys.odeint(LiSys.diffeq,Z,tau,tau+dtau,ctrl,NULL);
+        double ctrl = dtau/10;
+        LS.odeint(LS.diffeq,Z,tau,tau+dtau,ctrl,NULL);
         fp("%g %g %g %g\n", tau+dtau, Z[1], Z[2], Z[3]);
     }
 

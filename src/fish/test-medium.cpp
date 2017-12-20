ec4386aefaf78402beda6358cb119f91c4e7d5c5
ybouret <yann.bouret@gmail.com>
2017-12-20 09:13:22 +0100

    test-medium

--- src/fish/test-medium.cpp
+++ src/fish/test-medium.cpp
@@ -1,11 +1,46 @@
 #define MEDIUM_TEST 1
 #include "medium.h"
 #include "yocto/program.hpp"
+#include "yocto/code/alea.hpp"
 
 using namespace yocto;
 
+typedef Medium::NodeOf<int>  Node;
+typedef Medium::List< Node > List;
+typedef Medium::Pool< Node > Pool;
+
+#define NN 1000
 YOCTO_PROGRAM_START()
 {
+    alea.initialize();
+
+    List l;
+    Pool p;
+    for(size_t i=0;i<NN;++i)
+    {
+        p.store( new Node() );
+    }
+    if(p.size!=NN) throw exception("Pool error");
+
+    while(p.size>0)
+    {
+        if( alea.nextBool() )
+        {
+            l.push_back( p.query() );
+        }
+        else
+        {
+            l.push_front( p.query() );
+        }
+    }
+
+    while(l.size>0)
+    {
+        if( alea.nextBool() ) delete l.pop_front(); else delete l.pop_back();
+    }
+
+    Medium medium;
+
 
 }
 YOCTO_PROGRAM_END()

acef4169b0ef9f2189406cc926b9a478f6bf0233
ybouret <yann.bouret@gmail.com>
2018-11-14 14:35:07 +0100

    intake WIP

--- src/lithium/intake.cpp
+++ src/lithium/intake.cpp
@@ -4,6 +4,7 @@
 #include "y/math/round.hpp"
 #include "y/ios/ocstream.hpp"
 #include "y/os/progress.hpp"
+#include "y/string/convert.hpp"
 
 using namespace upsylon;
 using namespace math;
@@ -24,10 +25,10 @@ public:
     double k7;
     double Theta;
 
-    inline Intake() :
+    inline Intake(const double _Theta) :
     k6(1.1e-2),
     k7(1e-2),
-    Theta(4.7)
+    Theta(_Theta)
     {
     }
 
@@ -38,12 +39,25 @@ public:
         Y[I_B7] = 0;
     }
 
+    inline
+    double ratio( const Array &Y, const double t ) const
+    {
+        if(t<=0)
+        {
+            return k7/k6;
+        }
+        else
+        {
+            return Y[I_B7]/Y[I_B6];
+        }
+    }
+
     void compute( Array &dYdt, double t, const Array &Y)
     {
         const double ac   = Y[I_AC];
         const double b6   = Y[I_B6];
         const double b7   = Y[I_B7];
-        const double h    = get_h(t);
+        //const double h    = get_h(t);
 
         dYdt[I_AC]   = 0;
         dYdt[I_B6]   = k6*(Theta-b6);
@@ -59,19 +73,25 @@ private:
     Y_DISABLE_COPY_AND_ASSIGN(Intake);
 };
 
-static inline void save( ios::ostream &fp, const double t, const Array &fields )
+static inline void save( ios::ostream &fp, const double t, const Array &fields, const double r=0)
 {
     fp("%.15g",t);
     for(size_t i=1;i<=fields.size();++i)
     {
         fp(" %.15g", fields[i]);
     }
+    fp(" %.15g",r);
     fp << '\n';
 }
 
 Y_PROGRAM_START()
 {
-    Intake          intake;
+    double Theta = 4;
+    if(argc>1)
+    {
+        Theta = string_convert::to<double>(argv[1],"Theta");
+    }
+    Intake          intake(Theta);
     ODEquation      diffeq( &intake, & Intake::compute );
     ODE_Driver      odeint;
     odeint.start( Intake::NVAR );
@@ -79,17 +99,17 @@ Y_PROGRAM_START()
     odeint.eps = 1e-5;
 
     intake.setup(Y);
-    double t_max   = 180;
-    double dt      = 1e-3;
-    double dt_save = 1e-2;
+    double t_max   = 300;
+    double dt      = 0.01;
+    double dt_save = 0.1;
     const size_t every    = simulation_save_every(dt,dt_save);
     const size_t iters    = simulation_iter(t_max,dt,every);
     double h = dt/10;
-    const string filename = "output.dat";
+    const string filename = vformat("output_Theta%g.dat", intake.Theta);
     std::cerr << "running " << iters << " steps up to " << iters * dt << " seconds, saving every " << every << " steps" << std::endl;
     {
         ios::ocstream fp(filename);
-        //save(fp,0,Y);
+        save(fp,0,Y, intake.ratio(Y,0) );
     }
 
     progress bar;
@@ -103,7 +123,7 @@ Y_PROGRAM_START()
         if( 0 == (i%every) )
         {
             ios::ocstream fp(filename,true);
-            save(fp,t1,Y);
+            save(fp,t1,Y,intake.ratio(Y,t1));
             bar.update(i,iters);
             bar.display(std::cerr) << '\r';
         }

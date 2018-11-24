ab3459c074e44180c99cb20d3e43c964cea23b58
ybouret <yann.bouret@gmail.com>
2018-11-24 15:25:59 +0100

    need to make reverse setting

--- src/lithium/intake.cpp
+++ src/lithium/intake.cpp
@@ -23,51 +23,62 @@ public:
     static const size_t I_B6   = 2;
     static const size_t I_B7   = 3;
 
-    const double Theta;
-    const double d7in;
-    const double d7out;
-    const double sigma;
-    const double eps;
-    const double omeps; //!< 1-eps
-    const double k7;
-    const double k6;
-    const double k_eps;
-    const double catalytic;
-    const double kh;
-    const double Ua;
-    const double Ub;
 
+#if 0
     static double getSigma( const double d7In, const double d7Out )
     {
         return (1.0+0.001*d7Out)/(1.0+0.001*d7In);
     }
+#endif
+
+    const double Theta;
+    const double d7out;
+    const double eps6;
+    const double eps7;
+    const double U6;
+    const double U7;
+    const double Ua;
+    const double mu6;
+    const double mu7;
+    const double eta;
+    const double Q6;
+    const double Q7;
+    const double h_ini;
+    const double h_end;
+    const double scale;
+
+
 
     inline Intake(const double _Theta,
-                  const double _d7in,
-                  const double _d7out,
-                  const double _catalytic,
-                  const double _Ua,
-                  const double _Ub) :
+                  const double _d7out) :
     Theta( _Theta ),
-    d7in(_d7in),
     d7out(_d7out),
-    sigma( getSigma(d7in,d7out) ),
-    eps(1.0/(1.0+beta_s*(1.0+0.001*d7out))),
-    omeps(1.0-eps),
-    k7(1.0),
-    k6(sigma*k7),
-    k_eps(k6*eps+k7*omeps),
-    catalytic(_catalytic),
-    kh(catalytic*k7),
-    Ua(_Ua),
-    Ub(_Ub)
+    eps6(1.0/(1.0+beta_s*(1.0+0.001*d7out))),
+    eps7(1.0-eps6),
+    U6(0.1),
+    U7(0.1),
+    Ua(eps6*U6+eps7*U7),
+    mu6(0.5),
+    mu7(mu6),
+    eta(40.0),
+    Q6(1.1),
+    Q7(Q6),
+    h_ini(1),
+    h_end(0.1),
+    scale(0.5)
     {
         std::cerr << "Theta   = " << Theta << std::endl;
-        std::cerr << "sigma   = " << sigma << std::endl;
-        std::cerr << "epsilon = " << eps   << std::endl;
-        std::cerr << "k7      = " << k7    << std::endl;
-        std::cerr << "k6      = " << k6    << std::endl;
-        std::cerr << "k_eps   = " << k_eps << std::endl;
+        std::cerr << "eps6    = " << eps6  << std::endl;
+        std::cerr << "eps7    = " << eps7  << std::endl;
+        std::cerr << "U6      = " << U6    << std::endl;
+        std::cerr << "U7      = " << U7    << std::endl;
+        std::cerr << "Ua      = " << Ua    << std::endl;
+        std::cerr << "mu6     = " << mu6   << std::endl;
+        std::cerr << "mu7     = " << mu7   << std::endl;
+        std::cerr << "eta     = " << eta   << std::endl;
+        std::cerr << "Q6      = " << Q6    << std::endl;
+        std::cerr << "Q7      = " << Q7    << std::endl;
+
     }
 
     void setup( Array &Y )
@@ -77,12 +88,15 @@ public:
         Y[I_B7] = 0;
     }
 
+
     inline
-    double ratio( const Array &Y, const double t ) const
+    double ratio( const Array &Y, const double tau )
     {
-        if(t<=0)
+        if(tau<=0)
         {
-            return 1.0/sigma;
+            vector<double> dY( NVAR );
+            compute(dY,0,Y);
+            return dY[I_B7]/dY[I_B6];
         }
         else
         {
@@ -91,26 +105,26 @@ public:
     }
 
     inline
-    double delta7( const Array &Y, const double t ) const
+    double delta7( const Array &Y, const double t )
     {
         return 1000.0 * ( (1+0.001*d7out) * ratio(Y,t) - 1.0 );
     }
 
-    void compute( Array &dYdt, double, const Array &Y)
+    void compute( Array &dYdt, double tau, const Array &Y)
     {
         const double ac   = Y[I_AC];
         const double b6   = Y[I_B6];
         const double b7   = Y[I_B7];
-        //const double h    = get_h(t);
+        const double h    = getH(tau);
 
-        dYdt[I_AC]   = kh - ac * (kh+k_eps*Ua);
-        dYdt[I_B6]   = k6*( (Theta-b6) + ac * Ub );
-        dYdt[I_B7]   = k7*( (Theta-b7) + ac * Ub );
+        dYdt[I_AC]   = 1.0 -  ac*(1.0+h*Ua) + (1.0-ac)*(eps6*b6*Q6+eps7*b7*Q7);
+        dYdt[I_B6]   = mu6*(Theta - b6) + eta*( ac*h*U6 + (1-ac) * Q6 * b6 );
+        dYdt[I_B7]   = mu7*(Theta - b7) + eta*( ac*h*U7 + (1-ac) * Q7 * b7 );
     }
 
-    double get_h(double)
+    double getH(double tau)
     {
-        return pow(1.0,-5.9);
+        return h_ini + (h_end-h_ini)*(1.0-exp(-scale*tau));
     }
 
 private:
@@ -130,36 +144,18 @@ static inline void save( ios::ostream &fp, const double t, const Array &fields, 
 Y_PROGRAM_START()
 {
     double Theta     = 4;
-    double catalytic = 2;
-    double Ua        = 0;
-    double Ub        = 0;
     if(argc>1)
     {
         Theta = string_convert::to<double>(argv[1],"Theta");
     }
 
-    if( argc > 2 )
-    {
-        catalytic = string_convert::to<double>(argv[2],"catalytic");
-    }
-
-    if( argc > 3 )
-    {
-        Ua = string_convert::to<double>(argv[3],"Ua");
-    }
-
-    if( argc > 4 )
-    {
-        Ub = string_convert::to<double>(argv[4],"Ub");
-    }
-
 
 
     const double d7Out = 15.2;
-    const double d7In  = 1.05;
+    //const double d7In  = 1.05;
 
 
-    Intake          intake(Theta,d7In,d7Out,catalytic,Ua,Ub);
+    Intake          intake(Theta,d7Out);
 
     ODEquation      diffeq( &intake, & Intake::compute );
     ODE_Driver      odeint;
@@ -168,13 +164,13 @@ Y_PROGRAM_START()
     odeint.eps = 1e-5;
 
     intake.setup(Y);
-    double t_max   = 10;
+    double t_max   = 20;
     double dt      = 0.001;
     double dt_save = 0.01;
     const size_t every    = simulation_save_every(dt,dt_save);
     const size_t iters    = simulation_iter(t_max,dt,every);
     double h = dt/10;
-    const string filename = vformat("Theta%g_catalytic%g_Ua%g_Ub%g.dat", intake.Theta, intake.catalytic, intake.Ua, intake.Ub);
+    const string filename = "intake.dat"; //vformat("Theta%g_catalytic%g_Ua%g_Ub%g.dat", intake.Theta, intake.catalytic, intake.Ua, intake.Ub);
     std::cerr << "running " << iters << " steps up to " << iters * dt << " seconds, saving every " << every << " steps" << std::endl;
     {
         ios::ocstream fp(filename);

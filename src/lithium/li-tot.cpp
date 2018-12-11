4462e7d8b2146e5232e1c14dc4f1b432b6383b34
ybouret <yann.bouret@gmail.com>
2018-12-11 13:56:50 +0100

    ok for program

--- src/lithium/li-tot.cpp
+++ src/lithium/li-tot.cpp
@@ -8,11 +8,11 @@ using namespace math;
 
 Y_PROGRAM_START()
 {
-    const double Vcell = 2600 * ipower(1e-6,3) * 1000; //!< in liters
-    const double Ncell = 3.5e6;
-    const double Vtot  = Vcell*Ncell;
-    const double Vwork = 2e-3; //! in L
-
+    const double Vcell    = 2600 * ipower(1e-6,3) * 1000; //!< in liters
+    const double Ncell    = 3.5e6;
+    const double Vtot     = Vcell*Ncell;
+    const double Vwork    = 2e-3; //! in L
+    const double LiAllOut = 15e-3;
     std::cerr << "Vtot=" << Vtot << " L" << std::endl;
 
     size_t iCol = 2;
@@ -23,6 +23,11 @@ Y_PROGRAM_START()
     if(argc>1)
     {
         const string filename = argv[1];
+        if(argc>2)
+        {
+            iCol = string_convert::to<size_t>( argv[2], "iCol" );
+        }
+
         {
             math::data_set<double> ds;
             ds.use(1, t);
@@ -34,16 +39,17 @@ Y_PROGRAM_START()
         for(size_t i=1;i<=n;++i)
         {
             Li[i] = (Li[i]*1e-6*Vwork)/Vtot;
-            std::cerr << "t=" << t[i] << " => " << Li[i] << " M" << std::endl;
         }
 
         {
-            const string  outname = "li-all-out.txt";
+            const string  outname = "li-all-in.txt";
             ios::ocstream fp(outname);
             fp << "#t Li(M)\n";
             for(size_t i=1;i<=n;++i)
             {
-                fp("%.15g %.15g\n",t[i],Li[i]);
+                fp("%.15g %.15g %.15g\n",t[i],Li[i],Li[i]/LiAllOut);
+                std::cerr << "t=" << t[i] << " => " << Li[i] << " M" << " / " << LiAllOut << " = " << Li[i]/LiAllOut << std::endl;
+
             }
         }
     }

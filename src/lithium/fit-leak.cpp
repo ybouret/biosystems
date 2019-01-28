7e9f5a1d197ce6c0d69983d10afe40ccf22629a0
ybouret <yann.bouret@gmail.com>
2019-01-28 15:19:35 +0100

    fit with the possibility of different or same Theta's

--- src/lithium/fit-leak.cpp
+++ src/lithium/fit-leak.cpp
@@ -17,21 +17,35 @@ typedef array<double>           Array;
 typedef Fit::Variables          Variables;
 
 
+#if 0
 static const double beta_s = 12.0192;
 static const double d7out  = 14.57;
 static const double eps6   = 1.0/(1.0+beta_s*(1.0+0.001*d7out));
+#else
+static const double eps6 = 0;
+#endif
+
 static const double eps7   = 1.0 - eps6;
 static const double sigma  = 1.0/0.99772;
 
 static  double grow6(const double tau) { return 1.0-exp(-sigma*tau); }
 static  double grow7(const double tau) { return 1.0-exp(-tau);        }
-static  double grow(const double tau)  { return eps6 * grow6(tau) + eps7 * grow7(tau); }
+static  double grow( const double tau) { return eps6 * grow6(tau) + eps7 * grow7(tau); }
+
+static double grow6_linear(const double tau) { return sigma*tau; }
+static double grow7_linear(const double tau) { return       tau; }
+static double grow_linear(const double tau)  { return eps6 * grow6_linear(tau) + eps7 * grow7_linear(tau); }
+
+static double grow6_sq(const double tau) { return sigma*tau-square_of(sigma*tau)/2; }
+static double grow7_sq(const double tau) { return       tau-square_of(tau)/2; }
+static double grow_sq(const double tau)  { return eps6 * grow6_sq(tau) + eps7 * grow7_sq(tau); }
+
 
 class Leak
 {
 public:
 
-    explicit Leak() 
+    explicit Leak()
     {
     }
 
@@ -48,8 +62,30 @@ public:
         const double LiOut = vars(aorg,"Li");
 
         return LiOut * Theta * grow( k7 * t );
-
     }
+
+    double ComputeLinear(double           t,
+                         const Array     &aorg,
+                         const Variables &vars)
+    {
+        const double Theta = vars(aorg,"Theta");
+        const double k7    = vars(aorg,"k7");
+        const double LiOut = vars(aorg,"Li");
+
+        return LiOut * Theta * grow_linear( k7 * t );
+    }
+
+    double ComputeSq(double           t,
+                     const Array     &aorg,
+                     const Variables &vars)
+    {
+        const double Theta = vars(aorg,"Theta");
+        const double k7    = vars(aorg,"k7");
+        const double LiOut = vars(aorg,"Li");
+
+        return LiOut * Theta * grow_sq( k7 * t );
+    }
+
 
 private:
     Y_DISABLE_COPY_AND_ASSIGN(Leak);
@@ -140,22 +176,32 @@ Y_PROGRAM_START()
     Variables &vars = samples.variables;
     vars << "k7"; // global k7
 
+    bool useSameTheta      = false;
+    bool useDifferentTheta = !useSameTheta;
+
     // different Theta's
-    for(size_t i=1;i<=unique_labels.size();++i)
+    if(useSameTheta)
     {
+        for(size_t i=1;i<=unique_labels.size();++i)
         {
-            const string th = "Theta_" + unique_labels[i];
+            {
+                const string th = "Theta_" + unique_labels[i];
             vars << th;
+            }
         }
-
-
     }
+
 
     // different Li
     for(size_t i=1;i<=ns;++i)
     {
         const string Li = vformat("Li#%u", unsigned(i) );
         vars << Li;
+        if(useDifferentTheta)
+        {
+            const string Th = vformat("Theta#%u",unsigned(i));
+            vars << Th;
+        }
     }
 
 
@@ -175,12 +221,19 @@ Y_PROGRAM_START()
         Variables &local = samples[i]->variables;
         local("k7", vars["k7"]);
 
+        if(useSameTheta)
         {
-            const string th = "Theta_" + labels[i];
-            local("Theta",vars[th]);
-            local(aorg,"Theta") = 4.5;
-            local(used,"Theta") = true;
+            const string Th = "Theta_" + labels[i];
+            local("Theta",vars[Th]);
+            local(aorg,"Theta") = i;
         }
+        else
+        {
+            const string Th = vformat("Theta#%u",unsigned(i));
+            local("Theta",vars[Th]);
+            local(aorg,"Theta") = i;
+        }
+        local(used,"Theta") = true;
 
         {
             const string Li = vformat("Li#%u", unsigned(i) );
@@ -191,7 +244,7 @@ Y_PROGRAM_START()
         std::cerr << "  |_local[" << i << "]=" << local << std::endl;
     }
 
-    vars(aorg,"k7") = 0.01;
+    vars(aorg,"k7") = 0.002;
 
     std::cerr << "aorg=" << aorg << std::endl;
 
@@ -199,26 +252,60 @@ Y_PROGRAM_START()
     // preparing function
     Leak                                leak;
     Fit::LeastSquares<double>           LS;
-    Fit::LeastSquares<double>::Function F( &leak, & Leak::Compute );
 
     vars(used,"k7")=true;
 
-
-    if( !LS.fit(samples, F, aorg, aerr, used) )
     {
-        throw exception("couldn't fit");
-    }
-    std::cerr << "-- Results: " << std::endl;
-    const double R2 = samples.computeR2();
-    std::cerr << "R2=" << R2 << std::endl;
+        std::cerr << "== Fit Linear ==" << std::endl;
+        Fit::LeastSquares<double>::Function F( &leak, & Leak::ComputeLinear );
+        if( !LS.fit(samples, F, aorg, aerr, used) )
+        {
+            throw exception("couldn't fit");
+        }
+        std::cerr << "-- Results: " << std::endl;
+        const double R2 = samples.computeR2();
+        std::cerr << "R2=" << R2 << std::endl;
 
-    vars.display(std::cerr,aorg,aerr);
+        vars.display(std::cerr,aorg,aerr);
+    }
+
+    {
+        std::cerr << "== Fit Quadratic ==" << std::endl;
+        Fit::LeastSquares<double>::Function F( &leak, & Leak::ComputeSq );
+        if( !LS.fit(samples, F, aorg, aerr, used) )
+        {
+            throw exception("couldn't fit");
+        }
+        std::cerr << "-- Results: " << std::endl;
+        const double R2 = samples.computeR2();
+        std::cerr << "R2=" << R2 << std::endl;
+
+        vars.display(std::cerr,aorg,aerr);
+    }
+
+    {
+        std::cerr << "== Fit Full ==" << std::endl;
+        Fit::LeastSquares<double>::Function F( &leak, & Leak::Compute );
+        if( !LS.fit(samples, F, aorg, aerr, used) )
+        {
+            throw exception("couldn't fit");
+        }
+        std::cerr << "-- Results: " << std::endl;
+        const double R2 = samples.computeR2();
+        std::cerr << "R2=" << R2 << std::endl;
+
+        vars.display(std::cerr,aorg,aerr);
+    }
+
+
+
 
     for(size_t i=1;i<=ns;++i)
     {
+        std::cerr << "  |_Saving from " << files[i] << std::endl;
         string fitname = vfs::base_name_from(files[i]);
         vfs::change_extension(fitname, "fit.dat");
-        std::cerr << "Saving [" << fitname << "]" << std::endl;
+        std::cerr << "   |_Saving [" << fitname << "]" << std::endl;
         ios::ocstream fp(fitname);
         const Sample &s = *samples[i];
         for(size_t i=1;i<=s.X.size();++i)

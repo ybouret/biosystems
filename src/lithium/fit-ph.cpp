ea22eddac14b977bbb294f514ac1ea97116d7ba9
ybouret <yann.bouret@gmail.com>
2019-01-30 16:27:13 +0100

    WIP fit

--- src/lithium/fit-ph.cpp
+++ src/lithium/fit-ph.cpp
@@ -6,6 +6,7 @@
 #include "y/lang/pattern/matching.hpp"
 #include "y/math/io/data-set.hpp"
 #include "y/string/tokenizer.hpp"
+#include "y/fs/vfs.hpp"
 
 using namespace upsylon;
 using namespace math;
@@ -13,6 +14,41 @@ using namespace Lang;
 
 typedef vector<double>                Vector;
 typedef vector<string,memory::pooled> Strings;
+
+
+class Proton
+{
+public:
+    explicit Proton() throw()
+    {
+    }
+
+    virtual ~Proton() throw()
+    {
+    }
+
+    double Compute( double t, const array<double> &a, const Fit::Variables &vars )
+    {
+        const double vi = vars(a,"Hini");
+        const double ve = vars(a,"Hend");
+        const double t0 = vars(a,"t0");
+        if( t<= t0 )
+        {
+            return vi;
+        }
+        else
+        {
+            const double p = vars(a,"p");
+            const double q = vars(a,"q");
+            const double tp = pow(t-t0,p);
+            const double qp = pow(q,p);
+            return vi + (ve-vi) * tp/(tp+qp);
+        }
+    }
+
+private:
+    Y_DISABLE_COPY_AND_ASSIGN(Proton);
+};
 
 static bool isWS( const char C) { return ' ' == C || '\t' == C; }
 
@@ -33,10 +69,14 @@ public:
     double       Hmin;
     double       Hmax;
     double       Hend;
+    Vector       Y;
+    string       save_name;
+    Vector       Yf;
+    Fit::Sample<double> &sample;
 
     inline const string & key() const throw() { return Li; }
 
-    inline explicit Record( const string &fn ) :
+    inline explicit Record( const string &fn, Fit::Samples<double> &samples ) :
     file_name( fn ),
     Li( GetLiFromString(file_name) ),
     t(),
@@ -47,10 +87,13 @@ public:
     H(),
     Hmin(0),
     Hmax(0),
-    Hend(0)
+    Hend(0),
+    save_name( vfs::base_name_from(fn) ),
+    Yf(),
+    sample( samples.add(t,Y,Yf) )
     {
         std::cerr << "|_Record: [" << Li << "] from '" << file_name << "'" << std::endl;
-
+        vfs::change_extension(save_name,"dat");
         // Load data
         {
             data_set<double> ds;
@@ -93,14 +136,33 @@ public:
             }
         }
 
+        // Building fitting curve
+        Y.make(N,0);
+        Yf.make(N,0);
+        for(size_t i=1;i<=N;++i)
+        {
+            if(t[i]<=t0||i<=1)
+            {
+                Y[i] = Hmax;
+            }
+            else
+            {
+                assert(i>1);
+                Y[i] = min_of( H[i], Y[i-1] );
+                assert(Y[i]<=Y[i-1]);
+            }
+        }
+
         std::cerr << "| \\_Loaded #" << N << std::endl;
-        std::cerr << "| \\_pH_asymp = " << pH_asymp << std::endl;
-        std::cerr << "| \\_Hmin     = " << Hmin     << std::endl;
-        std::cerr << "| \\_Hmax     = " << Hmax     << std::endl;
-        std::cerr << "| \\_Hend     = " << Hend     << std::endl;
-        std::cerr << "| \\_t0       = " << t0       << std::endl;
+        std::cerr << "| \\_pH_asymp = " << pH_asymp  << std::endl;
+        std::cerr << "| \\_Hmin     = " << Hmin      << std::endl;
+        std::cerr << "| \\_Hmax     = " << Hmax      << std::endl;
+        std::cerr << "| \\_Hend     = " << Hend      << std::endl;
+        std::cerr << "| \\_t0       = " << t0        << std::endl;
+        std::cerr << "| \\_save     = " << save_name << std::endl;
         std::cerr << "|" << std::endl;
 
+        save();
     }
 
     inline virtual ~Record() throw()
@@ -116,6 +178,15 @@ public:
             throw exception("need one concentration in file name '%s'", *s);
         }
         return tokens.front().to_string(0,2);
+    }
+
+    void save() const
+    {
+        ios::ocstream fp(save_name);
+        for(size_t i=1;i<=N;++i)
+        {
+            fp("%g %g %g %g\n", t[i], H[i], Y[i], Yf[i] );
+        }
     }
 
 private:
@@ -135,16 +206,34 @@ public:
     inline explicit Records() throw() {}
     inline virtual ~Records() throw() {}
 
-    inline void add( const char *fn )
+    inline void add( const char *fn, Fit::Samples<double> &samples )
     {
-        const string _(fn);
-        const Record::Pointer p = new Record(_);
+        const string          _(fn);
+        const Record::Pointer p = new Record(_,samples);
         if( !insert(p) )
         {
             throw exception("Multiple concentration %s", *(p->Li));
         }
         sort_keys(compare_natural);
     }
+
+    void processAsymptote()
+    {
+        Vector Li(size(),as_capacity);
+        Vector H(size(),as_capacity);
+        {
+            ios::ocstream fp("asymp.dat");
+            for(iterator i=begin();i!=end();++i)
+            {
+                const Record &r = **i;
+                Li.push_back( string_convert::to<double>(r.Li,"Li") );
+                H. push_back( r.Hend );
+                fp("%.15g %.15g\n", Li.back(), r.Hmax-H.back() );
+            }
+
+        }
+    }
+
 
 private:
     Y_DISABLE_COPY_AND_ASSIGN(Records);
@@ -154,16 +243,134 @@ typedef Records::iterator Iterator;
 
 Y_PROGRAM_START()
 {
-    Records db;
+    Fit::Samples<double> samples;
+    Records              db;
     for(int i=1;i<argc;++i)
     {
-        db.add(argv[i]);
+        db.add(argv[i],samples);
     }
+
+    // building variables
+    Fit::Variables &gvars = samples.variables;
+    gvars << "p";// << "q";
+    for(Iterator i=db.begin();i!=db.end();++i)
+    {
+        Record         &r = **i;
+        Fit::Variables &lvars = r.sample.variables;
+        std::cerr << "using " << r.file_name << std::endl;
+
+        lvars("p",gvars);
+       // lvars("q",gvars);
+
+        static const char *vnames[] =
+        {
+            "q",
+            "Hini",
+            "Hend",
+            "t0"
+        };
+
+        for(size_t j=0;j<sizeof(vnames)/sizeof(vnames[0]);++j)
+        {
+            const string name = vnames[j] + r.Li;
+            gvars << name;
+            lvars(vnames[j],gvars[name]);
+        }
+
+        std::cerr << "lvars@" << r.Li << "=" << lvars << std::endl;
+    }
+
+    std::cerr << "gvars=" << gvars << std::endl;
+
+    // initializing variables
+    const size_t nv = gvars.size();
+
+    Vector       aorg( nv );
+    Vector       aerr( nv );
+    vector<bool> used(nv,false);
+
+    gvars.on(used,"p");
+    gvars(aorg,"p") = 1;
+    //gvars(aorg,"q") = 10.0;
 
     for(Iterator i=db.begin();i!=db.end();++i)
     {
-        std::cerr << "using " << (**i).file_name << std::endl;
+        Record         &r      = **i;
+        Fit::Variables &lvars = r.sample.variables;
+        lvars(aorg,"Hini") = r.Hmax;
+        lvars(aorg,"Hend") = r.Hend;
+        lvars(aorg,"t0"  ) = r.t0;
+        lvars(aorg,"q")    = 30;
+        lvars.on(used,"q");
     }
+
+    gvars.display(std::cerr,aorg);
+
+    Proton                      proton;
+    Fit::Type<double>::Function F( &proton, & Proton::Compute );
+    Fit::LeastSquares<double>   LS;
+
+    int level=0;
+    if( !LS.fit(samples, F, aorg, aerr, used) )
+    {
+        throw exception("couldn't fit level-%d",++level);
+    }
+
+    gvars.display(std::cerr,aorg,aerr);
+    for(Iterator i=db.begin();i!=db.end();++i)
+    {
+        Record         &r      = **i;
+        r.save();
+    }
+
+
+    if( true )
+    {
+        std::cerr << std::endl;
+        for(Iterator i=db.begin();i!=db.end();++i)
+        {
+            Record         &r      = **i;
+            Fit::Variables &lvars = r.sample.variables;
+            lvars.on(used,"Hini:Hend");
+        }
+        if( !LS.fit(samples, F, aorg, aerr, used) )
+        {
+            throw exception("couldn't fit level-%d",++level);
+        }
+        gvars.display(std::cerr,aorg,aerr);
+        for(Iterator i=db.begin();i!=db.end();++i)
+        {
+            Record         &r      = **i;
+            r.save();
+        }
+    }
+
+    if( true )
+    {
+        std::cerr << std::endl;
+        for(Iterator i=db.begin();i!=db.end();++i)
+        {
+            Record         &r      = **i;
+            Fit::Variables &lvars = r.sample.variables;
+            lvars.on(used,"t0");
+        }
+        if( !LS.fit(samples, F, aorg, aerr, used) )
+        {
+            throw exception("couldn't fit level-%d",++level);
+        }
+        gvars.display(std::cerr,aorg,aerr);
+        for(Iterator i=db.begin();i!=db.end();++i)
+        {
+            Record         &r      = **i;
+            r.save();
+        }
+    }
+
+    db.processAsymptote();
+
+
+
+
 
 }
 Y_PROGRAM_END()

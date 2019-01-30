73bf6d4d139d1a6edbbe12b23c901d059e220723
ybouret <yann.bouret@gmail.com>
2019-01-30 14:36:45 +0100

    WIP fit pH

--- src/lithium/fit-ph.cpp
+++ src/lithium/fit-ph.cpp
@@ -4,10 +4,17 @@
 #include "y/math/fit/ls.hpp"
 #include "y/ptr/intr.hpp"
 #include "y/lang/pattern/matching.hpp"
+#include "y/math/io/data-set.hpp"
+#include "y/string/tokenizer.hpp"
 
 using namespace upsylon;
 using namespace math;
 using namespace Lang;
+
+typedef vector<double>                Vector;
+typedef vector<string,memory::pooled> Strings;
+
+static bool isWS( const char C) { return ' ' == C || '\t' == C; }
 
 class Record : public counted_object
 {
@@ -17,14 +24,47 @@ public:
 
     const string file_name;
     const string Li;           //!< concentration string, used as key
+    Vector       t;
+    Vector       pH;
+    const size_t N;
+    double       t0;
+    double       pH_asymp;
 
     inline const string & key() const throw() { return Li; }
 
     inline explicit Record( const string &fn ) :
     file_name( fn ),
-    Li( GetLiFromString(file_name) )
+    Li( GetLiFromString(file_name) ),
+    t(),
+    pH(),
+    N(0),
+    t0(0),
+    pH_asymp(0)
     {
-        std::cerr << "Record: [" << Li << "] from '" << file_name << "'" << std::endl;
+        std::cerr << "|_Record: [" << Li << "] from '" << file_name << "'" << std::endl;
+
+        // Load data
+        {
+            data_set<double> ds;
+            ds.use(1,t);
+            ds.use(2,pH);
+            ios::icstream fp(file_name);
+            {
+                // reading first line
+                string line;
+                if(!fp.gets(line)) throw exception("missing first line in '%s'", *file_name);
+                Strings words;
+                tokenizer<char>::split(words,line,isWS);
+                if(words.size()<2) throw exception("invalid #tokens on first line");
+                pH_asymp = string_convert::to<double>( words[2], "pH_asymp" );
+            }
+            ds.load(fp);
+        }
+        (size_t&)N = t.size();
+        std::cerr << "| \\_Loaded #" << N << std::endl;
+        std::cerr << "| \\_pH_asymp=" << pH_asymp << std::endl;
+        std::cerr << "|" << std::endl;
+
     }
 
     inline virtual ~Record() throw()

08c4c851a3c7a2008061b7e2d0eb24d94f07f05a
ybouret <yann.bouret@gmail.com>
2019-03-11 14:38:36 +0100

    WIP

---
+++ src/lithium/simlin.cpp
@@ -0,0 +1,27 @@
+
+#include "y/program.hpp"
+#include "y/math/fit/ls.hpp"
+
+using namespace upsylon;
+using namespace math;
+
+class Lithium
+{
+public:
+    explicit Lithium() {}
+    virtual ~Lithium() throw() {}
+    
+    
+    
+private:
+    Y_DISABLE_COPY_AND_ASSIGN(Lithium);
+    
+};
+
+
+Y_PROGRAM_START()
+{
+    
+}
+Y_PROGRAM_END()
+

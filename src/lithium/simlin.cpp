923e1b7d913d41ee15a1e8e3ece043286481061e
ybouret <yann.bouret@gmail.com>
2019-03-15 10:54:00 +0100

    WIP simlin

--- src/lithium/simlin.cpp
+++ src/lithium/simlin.cpp
@@ -1,16 +1,29 @@
 
 #include "y/program.hpp"
-#include "y/math/fit/ls.hpp"
 #include "y/ios/ocstream.hpp"
+
+#include "y/math/timings.hpp"
+#include "y/math/ode/explicit/driver-ck.hpp"
+#include "y/math/fit/ls.hpp"
+
+#include "y/os/progress.hpp"
 
 using namespace upsylon;
 using namespace math;
 
 typedef Fit::Variables Variables;
+typedef array<double>  Array;
+typedef ODE::DriverCK<double>::Type   ODE_Driver;
+typedef ODE::Field<double>::Equation  ODEquation;
 
 class Lithium
 {
 public:
+    static const size_t I_AC = 1;
+    static const size_t I_B6 = 2;
+    static const size_t I_B7 = 3;
+    static const size_t NVAR = 3;
+    
     Variables      vars;
     vector<double> aorg;
     
@@ -22,10 +35,10 @@ public:
         vars << "pH_eta" << "pw_eta";
         
         aorg.make(vars.size(),0);
-        setup();
+        initialize();
     }
     
-    void setup()
+    void initialize()
     {
         Lithium &self = *this;
         self["pH_eta"] = 6.39;
@@ -44,7 +57,7 @@ public:
     
     inline double get_h(double t) const
     {
-        const Lithium &self = *this;
+        const Lithium &self  = *this;
         const double   h_ini = pow(10.0,-self["pH_ini"]);
         const double   h_end = pow(10.0,-self["pH_end"]);
         if(t<=0)
@@ -91,7 +104,19 @@ public:
         
     }
     
-
+    void Compute( array<double> &dY, double t, const array<double> &Y )
+    {
+        const double ac = Y[I_AC];
+        const double h  = get_h(t);
+        
+        dY[I_AC] = -ac;
+        
+    }
+    
+    void setup( Array &Y )
+    {
+        Y[I_AC] = 1;
+    }
     
 private:
     Y_DISABLE_COPY_AND_ASSIGN(Lithium);
@@ -101,13 +126,62 @@ private:
 
 Y_PROGRAM_START()
 {
-    Lithium Li;
+    Lithium    Li;
+    ODEquation  diffeq( &Li, & Lithium::Compute );
+    
+    ODE_Driver driver;
+    driver.eps = 1e-5;
     
     Li["pH_ini"] = 5.8;
     Li["pH_end"] = 6.2;
     Li["t_h"]    = 30.0;
     
     Li.save_info(5,7);
+    
+    
+    const double lt_min = -6;
+    double       lt_max =  log(10*60);
+    double       lt_amp = lt_max-lt_min;
+    double       lt_stp = 0.002;
+    double       lt_sav = 0.05;
+    size_t       every  = 0;
+    const size_t iters  = timings::setup(lt_amp, lt_stp, lt_sav, every);
+    lt_max = lt_amp + lt_min;
+    std::cerr << "#iters=" << iters  << ", saving every " << every << " lt_stp=" << lt_stp << " from " << lt_min << " to " << lt_max << std::endl;
+
+    vector<double> Y( Lithium::NVAR,0 );
+    
+    driver.start( Y.size() );
+    progress bar;
+    bar.start();
+    
+    vector<double> dY(Y.size());
+
+    double t0   = 0;
+    double ctrl = exp(lt_min)/10;
+    for(size_t i=1;i<=iters;++i)
+    {
+        const double lt1 = lt_min + ( (i-1)*lt_amp )/(iters-1);
+        const double t1  = exp(lt1);
+        driver( diffeq, Y, t0, t1, ctrl, NULL);
+        if(1==i||0==(i%every))
+        {
+            bar.update(i,iters);
+            bar.display(std::cerr) << '\r';
+#if 0
+            {
+                ios::ocstream fp("output.dat",true);
+                save(fp,lt1,Y);
+            }
+            {
+                ios::ocstream fp("li.dat",true);
+                fp("%.15g %.15g %.15g %.15g\n",lt1,Li.computeDelta(Y),Li.computeLiAllRatio(Y),exp(lt1));
+            }
+#endif
+        }
+        t0 = t1;
+    }
+    std::cerr << std::endl;
     
 }
 Y_PROGRAM_END()

0ca9a74ed19943817e8174cf1bb3ea412ca17b70
ybouret <yann.bouret@gmail.com>
2019-05-16 11:57:34 +0200

    WIP fitting PS120

--- src/lithium/simpot.cpp
+++ src/lithium/simpot.cpp
@@ -5,6 +5,7 @@
 #include "y/math/fit/ls.hpp"
 #include "y/math/fit/samples-io.hpp"
 #include "y/type/physics.hpp"
+#include "y/sort/heap.hpp"
 
 using namespace upsylon;
 using namespace math;
@@ -23,10 +24,16 @@ static double Lambda   = 15.0;
 
 static double Texp     = 37.0;
 
+static inline double V2Z( const double V )
+{
+    return (Y_FARADAY*V)/(Y_R*(Y_ZERO+Texp));
+}
+
 static inline double V2Theta( const double V )
 {
     return exp( -(Y_FARADAY*V)/(Y_R*(Y_ZERO+Texp)) );
 }
+
 
 static inline double Theta2V( const double th )
 {
@@ -37,21 +44,22 @@ class SimPot : public ProblemType
 {
 public:
     typedef IODE::Embedded<SimPot>::Type Alias;
-    static const size_t I_B7 = 1;
-    static const size_t I_B6 = 2;
+    static const size_t I_B7   = 1;
+    static const size_t I_B6   = 2;
+    static const size_t I_V    = 3;
 
     const double eps6;
     const double eps7;
     double k7;
     double k6;
-    double Theta0;
-    double u;
-    double coeff;
+    double V0;
+    double km;
+    double ym;
 
     explicit SimPot() :
     eps6(1.0/(1.0+lambda_s*(1.0+1.0e-3*d7out))),
     eps7(1.0-eps6),
-    k7(1e-2), k6(sigma*k7), Theta0( V2Theta(-40e-3) ), u(0), coeff(1)
+    k7(0), k6(sigma*k7), V0(0), km(0), ym(0)
     {
     }
 
@@ -59,20 +67,22 @@ public:
     {
         k7     = vars( aorg, "k7" ) * 1e-4;
         k6     = sigma * k7;
-        Theta0 = vars(aorg,"Theta0");
-        u      = vars(aorg,"u");
-        coeff  = vars(aorg,"coeff");
+        V0     = vars(aorg,"V0");
+        km     = vars(aorg,"km");
+        ym     = vars(aorg,"ym");
     }
 
     virtual ~SimPot() throw()
     {
     }
 
-    virtual size_t dimension() const throw() { return 2; }
+    virtual size_t dimension() const throw() { return 3; }
+
     virtual void   setup( Array &Y ) const throw()
     {
-        Y[1] = 0;
-        Y[2] = 0;
+        Y[I_B6] = 0;
+        Y[I_B7] = 0;
+        Y[I_V ] = V0;
     }
 
     virtual double begin() const throw() { return 0;   }
@@ -86,19 +96,21 @@ public:
 
     double  LiTot(const Array &Y ) const
     {
-        return Lambda * coeff * beta_of(Y);
+        return Lambda *  beta_of(Y);
     }
 
     virtual void compute( Array &dYdt, double, const Array &Y )
     {
         const double beta7 = Y[I_B7];
         const double beta6 = Y[I_B6];
+        const double V     = Y[I_V];
+        const double Theta = V2Theta(V);
         const double beta  = eps6*beta6 + eps7*beta7;
-        const double Theta = Theta0*exp(-u*beta);
 
-        dYdt[I_B7] = k7*(Theta - beta7);
-        dYdt[I_B6] = k6*(Theta - beta6);
-
+        const double db7 = dYdt[I_B7] = k7*(Theta - beta7);
+        const double db6 = dYdt[I_B6] = k6*(Theta - beta6);
+        const double dbeta = eps6*db6+eps7*db7;
+        dYdt[I_V]  = km*(V0-V)+ym*dbeta;
     }
 
 private:
@@ -129,11 +141,33 @@ public:
     }
 
 
+    void save(const double tmax)
+    {
+        SimPot &self = **this;
+        iode.reset();
+
+        {
+            ios::ocstream fp("beta.dat");
+            for(double x=0;x<=tmax; x += 5 )
+            {
+                const Array &Y = iode.update(x);
+                fp("%g %g %g\n", x, self.LiTot(Y), Y[self.I_V]*1000.0 );
+            }
+        }
+    }
+
 private:
     Y_DISABLE_COPY_AND_ASSIGN(Leak);
 };
 
 
+#define FIT_SESSION() do {\
+++level; std::cerr << "Fit Session #" << level << std::endl;\
+vars.display(std::cerr,used,"\tusing ");\
+if(!ls.fit(sample, F, aorg, aerr, used) ) throw exception("error in fit level-%d", level);\
+vars.display(std::cerr, aorg, aerr, "\t");\
+leak.save(t_sim);\
+} while(false)
 
 Y_PROGRAM_START()
 {
@@ -150,9 +184,12 @@ Y_PROGRAM_START()
     Fit::IO::Load(argv[1], 1,t, 2,C, Cf);
     const size_t N = t.size();
     std::cerr << "Loaded #" << N << std::endl;
+    hsort(t,C,comparison::increasing<double>);
+
+    const double t_sim = 2.0 * t[N];
 
     Variables & vars = sample.variables;
-    vars << "k7" << "Theta0" << "u" << "coeff";
+    vars << "k7" << "V0" << "km" << "ym";
 
     Solver        solver   = ODE::DriverCK<double>::New();
     solver->eps = 1e-5;
@@ -166,10 +203,10 @@ Y_PROGRAM_START()
     vector<double> aerr( nvar, 0 );
     vector<bool>   used( nvar, false );
 
-    vars(aorg,"k7")     = 3;
-    vars(aorg,"Theta0") = V2Theta( -40e-3 );
-    vars(aorg,"u")      = 0.0;
-    vars(aorg,"coeff")  = 1.0;
+    vars(aorg,"k7") = 3;
+    vars(aorg,"V0") = -40e-3;
+    vars(aorg,"km") = 0.001;
+    vars(aorg,"ym") = 0.0;
 
     Fit::LeastSquares<double> ls;
 
@@ -180,23 +217,38 @@ Y_PROGRAM_START()
 
     std::cerr << "Saving points..." << std::endl;
     {
-        ios::ocstream fp("beta.dat");
-        for(size_t i=1;i<=N;++i)
-        {
-            fp("%g %g %g\n", t[i], C[i], F(t[i],aorg,vars) );
-        }
-
-        leak.iode.reset();
-
-        {
-            ios::ocstream fp("bfit.dat");
-            for(double x=0;x<=1.5*t[N]; x += 10 )
-            {
-                fp("%g %g\n", x, leak->LiTot( leak.iode.update(x) ));
-            }
-        }
+        leak.save(t_sim);
     }
 
+
+    int level = 0;
+    vars.only_on(used,"k7");
+    FIT_SESSION();
+
+    vars.only_on(used,"ym");
+    FIT_SESSION();
+
+
+    vars.only_on(used,"k7");
+    FIT_SESSION();
+
+    vars.only_on(used,"k7:ym");
+    FIT_SESSION();
+
+    vars.only_on(used,"km");
+    FIT_SESSION();
+
+    vars.only_on(used,"km:ym");
+    FIT_SESSION();
+
+    vars.only_on(used,"k7");
+    FIT_SESSION();
+
+    vars.only_on(used,"k7:ym");
+    FIT_SESSION();
+
+    vars.only_off(used,"V0");
+    FIT_SESSION();
 
 
 #if 0
